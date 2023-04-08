ac9476c515dae9d16e5d7a71c3f87489eeaec1f6
Akshit Ostwal <akshitostwal@gmail.com>
2023-04-08 17:29:17 +0530

    few more configs changes

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,22 @@
+import 'package:wallet_connect_dart_v2/core/models/app_metadata.dart';
+
 class Config {
   const Config._();
   static const registryApiBaseUrl = "https://api-a.meroku.store";
   static const glApiBaseUrl = "https://htc.guardiannft.net";
+  static const sentryDSN =
+      "https://6b758247cdb149a1950fd476f793df98@o4504921352372224.ingest.sentry.io/4504921353486336";
+}
+
+class WalletConnectConfig {
+  const WalletConnectConfig._();
+  static const projectId = "36f352c5daeb6ed6ae15657366a9df3d";
+  static const relayUrl = "wss://relay.walletconnect.com";
+  static const metadata = AppMetadata(
+    name: 'DappStore_test',
+    description: 'Dapp Store by HTC',
+    url: 'https://htc.com/',
+    icons: ['https://1000logos.net/wp-content/uploads/2021/05/HTC-logo.png'],
+  );
+  static const database = 'memory';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dappstore/config/config.dart';
 import 'package:dappstore/core/application/app.dart';
 import 'package:dappstore/core/application/init/init.dart';
 import 'package:flutter/material.dart';
@@ -8,8 +9,7 @@ void main() async {
   await initialise();
   await SentryFlutter.init(
     (options) {
-      options.dsn =
-          'https://6b758247cdb149a1950fd476f793df98@o4504921352372224.ingest.sentry.io/4504921353486336';
+      options.dsn = Config.sentryDSN;
       // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
       // We recommend adjusting this value in production
       options.sendDefaultPii = false;
